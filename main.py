7cf6cb5728292dc3884fbd484e081d0c1ad2b3c0
ShrinkingViolet <1559650411@qq.com>
2020-12-19 22:08:32 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 import re
 # main
-with open('proxy.pac', 'r') as f:
-    proxy = re.findall(r'  "(.*)"',f.read())
+with open('database.qaq','r') as f:
+    database = re.findall(r'  "(.*)"',f.read())
+database.sort(reverse = True)
+with open('proxy.pac','w') as f:
+    wt = ''
+    for i in database:
+        wt = '''    "''' + i + '''": 1,\n''' + wt
+    with open('proxy.pac.tmp', 'r') as t:
+        f.write(t.read().replace('$$--$$', wt))
 
 with open('index.html', 'w') as f:
     wt = ''
-    for i in proxy:
+    for i in database:
         wt = '''<span class="line">    <span class="attr">&quot;'''+i+'''&quot;:</span> <span class="number">1</span>,</span><br>''' + wt
     with open('index.html.tmp', 'r') as t:
         f.write(t.read().replace('$$--$$', wt))
